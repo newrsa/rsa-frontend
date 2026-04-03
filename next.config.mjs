7eb43d37947b122de5eee78cf0e91b763534de57
RSAPradipSalke <pradip.salke@rightstepahead.com>
2026-04-03 23:15:23 +0530

    cicd config chnage

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,10 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactCompiler: true,
   output: "export",
+  images: {
+    unoptimized: true,
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
