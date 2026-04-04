8a31c86d79a0b63ab99c31e687bd806d824e40bd
RSAPradipSalke <pradip.salke@rightstepahead.com>
2026-04-04 14:15:36 +0530

    stop aos

--- src/app/layout.js
+++ src/app/layout.js
@@ -33,7 +33,7 @@ export default function RootLayout({ children }) {
 
         {/* Bootstrap JS */}
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-        <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+        {/* <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script> */}
       </body>
     </html>
   );
